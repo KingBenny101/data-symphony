71fd64d1af4cd56cb3fa31adb54d24f4ce0d4e6f
KingBenny101 <benstin2727@gmail.com>
2022-01-09 17:14:39 +0530

    Deploying in Heroku

--- DataSymphony/DataSymphony/settings.py
+++ DataSymphony/DataSymphony/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1o(2+054vx*43a#e9)i6#fkl!#gcq%5t!4k_9rsmywxnefkl%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
